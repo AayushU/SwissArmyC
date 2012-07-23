f63036295aeffdd24b6302a85786a911b3a88908
Aayush Upadhyay <aayushu@gmail.com>
2012-07-23 12:55:17 -0300

    Moved the strlen() out of the loop.

--- CUtilities/String/Reverse.c
+++ CUtilities/String/Reverse.c
@@ -1,8 +1,9 @@
 void reverse(char s[])
 {
     int c, i, j;
+    int len = strlen(s)
 
-    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+    for (i = 0, j = len-1; i < j; i++, j--) {
         c = s[i];
         s[i] = s[j];
         s[j] = c;
