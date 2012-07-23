2fc075cc97189c81d14f863bba1a2a62dfb1151e
Daniel Tahara <dan.tahara@gmail.com>
2012-07-22 18:23:07 -0700

    Add IO Utilities

---
+++ CUtilities/IO/PrintArray.c
@@ -0,0 +1,8 @@
+/* prints an array of ints of a given length */
+void printarr(int arr[], int length)
+{
+    for (int i = 0; i < length; i++) {
+        printf("%d ", anrr[i]);
+    }
+    putchar('\n');
+}
